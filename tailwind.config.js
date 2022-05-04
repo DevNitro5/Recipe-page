ca1f449b74d2a7cc0910bbc2a3343a94c181d608
Anish Chhetri <anishchhetri196@gmail.com>
2022-05-04 19:12:07 +0545

    Project 2

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: "'Montserrat'",
+        playfairDisplay: "'Playfair Display'",
+      },
+      fontSize: {
+        vvs: ".580rem",
+        tiny: "1rem",
+      },
+    },
+  },
+  plugins: [],
+};
